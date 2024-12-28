78825d5535b4382201de5796712ef890d92ea8c5
mfoniso <mfonisoischris@gmail.com>
2024-12-28 02:51:37 +0100

    feat: added skeleton procedure and updated templates

--- templates/nextjs/src/common/hooks/useToggleContext.ts
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import { ToggleContext } from "@/common/contexts/Toggle/Context";
-
-export default function useToggleContext() {
-  return useContext(ToggleContext);
-}
