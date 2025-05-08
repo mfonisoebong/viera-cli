9f44649713b13d0676617652cb9e71c217be1fff
mfoniso <mfonisoischris@gmail.com>
2025-05-08 20:10:50 +0100

    replaced prettier with biome as default formatter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
     node: true,
     es2021: true,
   },
-  extends: ["eslint:recommended", "plugin:prettier/recommend"],
+  extends: ["eslint:recommended"],
   overrides: [
     {
       env: {
@@ -18,8 +18,5 @@ module.exports = {
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
-  },
-  rules: {
-    "prettier/prettier": "error",
   },
 };
